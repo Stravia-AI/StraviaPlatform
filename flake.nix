cfe08ded337db36f47a51bc2021f5013dd1c9c2b
Chikage0o0 <chikage@939.me>
2026-09-11 02:40:57 +0800

    校正 Nix Cargo 依赖哈希

--- flake.nix
+++ flake.nix
@@ -144,7 +144,7 @@
             inherit version;
             src = source;
 
-            cargoHash = "sha256-NGM5FREiCPAbfUU34brE/kt0b8AteazEEDcjOUYDdJk=";
+            cargoHash = "sha256-R3MeoGJ3iFiJ5xYbWsZ4GzBf4nQVO0gaT69nZr98Lz0=";
             # 此包链接预编译 V8；移除源构建子模块，让 Moli 扩展选择公共头文件回退路径。
             cargoDepsHook = ''
               cargoDepsPrebuilt="$TMPDIR/00000000000000000000000000000000-cargo-deps-prebuilt"
